13fb9ebbf71b1df89e1820d951f39660ec9e8615
ashupednekar <ashutoshpednekar15@gmail.com>
2025-11-26 22:12:02 +0530

    feat: initial commit, defined spec types

---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/ashupednekar/hdata-encoder/cmd"
+
+func main() {
+	cmd.Execute()
+}
---
+++ pkg/spec.go
@@ -0,0 +1,16 @@
+package pkg
+
+type Value interface{
+	isValue()
+}
+
+type Str string
+type I32 int32
+type DataInput []Value
+
+func (_ Str) isValue() {}
+func (_ I32) isValue() {}
+func (_ DataInput) isValue() {}
+
+
+
